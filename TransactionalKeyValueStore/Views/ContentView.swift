e615e4a6c9c67bd04cc2e331566017997a494254
zakbarlow1995 <zak@stormideas.com>
2023-02-13 22:01:43 +0000

    Tidy up

--- TransactionalKeyValueStore/Views/ContentView.swift
+++ TransactionalKeyValueStore/Views/ContentView.swift
@@ -9,58 +9,56 @@ struct ContentView: View {
     }
 
     var body: some View {
-        ZStack {
-            VStack {
-                Spacer()
-                TextEditor(text: $viewModel.consoleText)
-                    .accessibilityIdentifier(viewModel.consoleAccessibilityIdentifier)
-                    .textSelection(.disabled)
+        VStack {
+            Spacer()
+            TextEditor(text: $viewModel.consoleText)
+                .accessibilityIdentifier(viewModel.consoleAccessibilityIdentifier)
+                .textSelection(.disabled)
 
-                ForEach($viewModel.inputModels) { $inputModel in
-                    HStack {
-                        ForEach(0..<inputModel.textFields.count, id: \.self) { index in
-                            TextField("", text: $inputModel.textFields[index], prompt: Text(inputModel.kind.placeholders[index]))
-                                .accessibilityIdentifier(inputModel.textFieldAccessibilityIdentifier(at: index))
-                                .textFieldStyle(.roundedBorder)
-                                .textInputAutocapitalization(.never)
-                        }
-                        Button {
-                            viewModel.actionFor(inputModel.kind)
-                        } label: {
-                            Text(inputModel.kind.rawValue)
-                        }
-                        .accessibilityIdentifier(inputModel.buttonAccessibilityIdentifier)
-                        .buttonStyle(.borderedProminent)
-                    }
-                }
-
+            ForEach($viewModel.inputModels) { $inputModel in
                 HStack {
-                    ForEach(0..<viewModel.commandButtons.count) { index in
-                        let buttonKind = viewModel.commandButtons[index]
-                        Button {
-                            viewModel.actionFor(buttonKind)
-                        } label: {
-                            Text(buttonKind.rawValue)
-                        }
-                        .accessibilityIdentifier(buttonKind.accessibilityIdentifier)
-                        .buttonStyle(.borderedProminent)
+                    ForEach(0..<inputModel.textFields.count, id: \.self) { index in
+                        TextField("", text: $inputModel.textFields[index], prompt: Text(inputModel.kind.placeholders[index]))
+                            .accessibilityIdentifier(inputModel.textFieldAccessibilityIdentifier(at: index))
+                            .textFieldStyle(.roundedBorder)
+                            .textInputAutocapitalization(.never)
                     }
-                }
-
-                HStack {
-                    ForEach(0..<viewModel.additionalCommandButtons.count) { index in
-                        let buttonKind = viewModel.additionalCommandButtons[index]
-                        Button {
-                            viewModel.actionFor(buttonKind)
-                        } label: {
-                            Text(buttonKind.rawValue)
-                        }
-                        .accessibilityIdentifier(buttonKind.accessibilityIdentifier)
-                        .buttonStyle(.borderedProminent)
+                    Button {
+                        viewModel.actionFor(inputModel.kind)
+                    } label: {
+                        Text(inputModel.kind.rawValue)
                     }
+                    .accessibilityIdentifier(inputModel.buttonAccessibilityIdentifier)
+                    .buttonStyle(.borderedProminent)
                 }
-                Spacer(minLength: 30)
             }
+
+            HStack {
+                ForEach(0..<viewModel.commandButtons.count) { index in
+                    let buttonKind = viewModel.commandButtons[index]
+                    Button {
+                        viewModel.actionFor(buttonKind)
+                    } label: {
+                        Text(buttonKind.rawValue)
+                    }
+                    .accessibilityIdentifier(buttonKind.accessibilityIdentifier)
+                    .buttonStyle(.borderedProminent)
+                }
+            }
+
+            HStack {
+                ForEach(0..<viewModel.additionalCommandButtons.count) { index in
+                    let buttonKind = viewModel.additionalCommandButtons[index]
+                    Button {
+                        viewModel.actionFor(buttonKind)
+                    } label: {
+                        Text(buttonKind.rawValue)
+                    }
+                    .accessibilityIdentifier(buttonKind.accessibilityIdentifier)
+                    .buttonStyle(.borderedProminent)
+                }
+            }
+            Spacer(minLength: 30)
         }
     }
 }
